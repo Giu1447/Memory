995e153bb845e735dbfc074a2ec29397c7702073
Giu1447 <giulian.mazzeo@lernende.bbw.ch>
2024-03-14 18:46:31 +0100

    React geht nicht

--- React/src/App.js
+++ React/src/App.js
@@ -1,100 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; 
+    import React, { useState, useEffect } from 'react';
+    import './App.css'; 
 
-function App() {
-    const [counter, setCounter] = useState(0);
-    const [checkedCounter, setCheckedCounter] = useState(0);
-    const [pictures, setPictures] = useState([]);
+    function App() {
+        const [counter, setCounter] = useState(0);
+        const [checkedCounter, setCheckedCounter] = useState(0);
+        const [pictures, setPictures] = useState([]);
 
-    useEffect(() => {
-        createPictures();
-    }, []);
+        useEffect(() => {
+            createPictures();
+        }, []);
 
-    function createPictures() {
-        const numbers = [...Array(9).keys()].map(x => x + 1);
-        const double = numbers.concat(numbers);
-        shuffleArray(double);
+        function createPictures() {
+            const numbers = [...Array(9).keys()].map(x => x + 1);
+            const double = numbers.concat(numbers);
+            shuffleArray(double);
 
-        const newPictures = double.map((number, index) => (
-            <div key={index} className={`picture flipped-${number}`} onClick={handlePictureClick}></div>
-        ));
+            const newPictures = double.map((number, index) => (
+                <div key={index} className={`picture flipped-${number}`} onClick={handlePictureClick}></div>
+            ));
 
-        setPictures(newPictures);
-    }
-
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
+            setPictures(newPictures);
         }
-    }
-    
-    function resetPictures() {
-        setPictures(pictures.map(picture => {
-            picture.props.className = picture.props.className.replace('clicked', '');
-            return picture;
-        }));
-        setCounter(0);
-    }
-    
-    function checkMatch() {
-        const clickedPictures = pictures.filter(picture => picture.props.className.includes('clicked'));
-        const first = clickedPictures[0].props.className.split(' ')[1];
-        const second = clickedPictures[1].props.className.split(' ')[1];
-    
-        if (first === second) {
+
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        }
+        
+        function resetPictures() {
             setPictures(pictures.map(picture => {
-                if (picture.props.className.includes('clicked')) {
-                    picture.props.style = { opacity: 0, cursor: 'default' };
-                }
+                picture.props.className = picture.props.className.replace('clicked', '');
                 return picture;
             }));
-            setCheckedCounter(prevCheckedCounter => prevCheckedCounter + clickedPictures.length);
-            wonGame();
+            setCounter(0);
         }
-    }
-    
-    function wonGame() {
-        if (checkedCounter === 18) {
-            const winMessage = <h1 className="win">You won!</h1>;
-            setPictures([winMessage]);
-            setCheckedCounter(0);
-        }
-    }
-
-    function handlePictureClick(event) {
-        const clickedPicture = event.target;
         
-        if (counter < 2) {
-            if (!clickedPicture.classList.contains('clicked')) {
-                clickedPicture.classList.add('clicked');
-                setCounter(prevCounter => prevCounter + 1);
-            } else {
-                clickedPicture.classList.remove('clicked');
-                setCounter(prevCounter => prevCounter - 1);
+        function checkMatch() {
+            const clickedPictures = pictures.filter(picture => picture.props.className.includes('clicked'));
+            const first = clickedPictures[0].props.className.split(' ')[1];
+            const second = clickedPictures[1].props.className.split(' ')[1];
+        
+            if (first === second) {
+                setPictures(pictures.map(picture => {
+                    if (picture.props.className.includes('clicked')) {
+                        picture.props.style = { opacity: 0, cursor: 'default' };
+                    }
+                    return picture;
+                }));
+                setCheckedCounter(prevCheckedCounter => prevCheckedCounter + clickedPictures.length);
+                wonGame();
             }
-        } 
-        
-        if (counter === 2) {
-            setTimeout(() => {
-                checkMatch();
-                resetPictures();
-            }, 1500);
         }
-    }
+        
+        function wonGame() {
+            if (checkedCounter === 18) {
+                const winMessage = <h1 className="win">You won!</h1>;
+                setPictures([winMessage]);
+                setCheckedCounter(0);
+            }
+        }
 
-    return (
-        <div className="content">
-            <div className="sideBar">
-                <div id="shuffleBtn">
-                    <button onClick={createPictures}>Shuffle Cards</button>
+        function handlePictureClick(event) {
+            const clickedPicture = event.target;
+            
+            if (counter < 2) {
+                if (!clickedPicture.classList.contains('clicked')) {
+                    clickedPicture.classList.add('clicked');
+                    setCounter(prevCounter => {
+                        console.log(prevCounter + 1); 
+                        return prevCounter + 1;
+                    });
+                } else {
+                    clickedPicture.classList.remove('clicked');
+                    setCounter(prevCounter => {
+                        console.log(prevCounter - 1); 
+                        return prevCounter - 1;
+                    });
+                }
+            } 
+            
+            if (counter === 2) {
+                setTimeout(() => {
+                    checkMatch();
+                    resetPictures();
+                }, 1500);
+            }
+        }
+        
+
+        return (
+            <div className="content">
+                <div className="sideBar">
+                    <div id="shuffleBtn">
+                        <button onClick={createPictures}>Shuffle Cards</button>
+                    </div>
+                </div>
+                <div id="pictures" className="pictures">
+                    {pictures}
                 </div>
             </div>
-            <div id="pictures" className="pictures">
-                {pictures}
-            </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default App;
+    export default App;
